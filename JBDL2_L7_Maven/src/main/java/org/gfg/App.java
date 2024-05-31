2b9f2f68073b0930563153f7af1a8623346fe806
Robin <31219114+robingautam@users.noreply.github.com>
2024-06-01 00:42:48 +0530

    Add files via upload

---
+++ JBDL2_L7_Maven/src/main/java/org/gfg/App.java
@@ -0,0 +1,22 @@
+package org.gfg;
+
+import com.google.gson.Gson;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Hello world!
+ *
+ */
+/**
+ *  This class is used for maven test
+ * **/
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Gson gson = new Gson();
+        System.out.println( "Hello World!" );
+
+    }
+}
